f9bf1b6a52bee91e9339323dcf0cd10dcf04402c
Matias Sabbadini <matiassabbadini24@gmail.com>
2023-08-14 18:19:42 -0300

    generate structure

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
-   {
-    path: '',
-    redirectTo: '',
-    pathMatch: 'full',
-  },
   {
     path: '',
     component: HomeComponent,
-    pathMatch: 'full',
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeRoutingModule { }
